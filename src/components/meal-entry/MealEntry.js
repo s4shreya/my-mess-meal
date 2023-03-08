3ae4e740ad04df61600109895cbc9775df489296
unknown <s4shreyadhingra@gmail.com>
2023-03-08 23:45:19 +0530

    added meal entry form

--- src/components/meal-entry/MealEntry.js
+++ src/components/meal-entry/MealEntry.js
@@ -1,21 +1,44 @@
-import { useState } from 'react';
+import { useState } from "react";
+import { ToastContainer, toast } from "react-toastify";
 
-import styles from '../../user-interface/Card.module.css';
-import MealEntryForm from './MealEntryForm';
+import "../../../node_modules/react-toastify/dist/ReactToastify.css";
+import styles from "../../user-interface/Card.module.css";
+import MealEntryForm from "./MealEntryForm";
 
 function MealEntry() {
-    const [ isModalOpen, setIsModalOpen ] = useState(false);
+  const [isModalOpen, setIsModalOpen] = useState(false);
 
-    const showMealEntryForm = () => setIsModalOpen(true);
+  const showMealEntryForm = () => setIsModalOpen(true);
 
-    const closeMealEntryForm = () => setIsModalOpen(false);
+  const closeMealEntryForm = () => setIsModalOpen(false);
 
-    return (
-        <div>
-            <h1 className={styles.card_title} onClick={showMealEntryForm}>Meal Entry</h1>
-            { isModalOpen && <MealEntryForm isModalOpen={isModalOpen} closeHandler={closeMealEntryForm} />}
-        </div>
+  const showSubmitMessage = (response) => {
+    const data = response ? "have" : "not have";
+    toast.success(
+      `Response submitted successfully.\nYou will ${data} the upcoming meal!`,
+      {
+        position: toast.POSITION.TOP_CENTER,
+        color: "success",
+      }
     );
+    closeMealEntryForm();
+  };
+
+  return (
+    <div>
+      <h1 className={styles.card_title} onClick={showMealEntryForm}>
+        Meal Entry
+      </h1>
+      {isModalOpen && (
+        <MealEntryForm
+          isModalOpen={isModalOpen}
+          closeHandler={closeMealEntryForm}
+          showSubmitMessage={showSubmitMessage}
+        />
+      )}
+      <ToastContainer />
+    </div>
+  );
 }
 
 export default MealEntry;
--- src/components/meal-entry/MealEntryForm.js
+++ src/components/meal-entry/MealEntryForm.js
@@ -54,7 +54,7 @@ function MealEntryFormModal(props) {
     if (menuCtxt.meal === 0 && menuCtxt.day !== 0) {
       menuCtxt.updateTotalCalories(cal);
     }
-    props.onHide();
+    props.showSubmitMessage(studentResponse.response);
   };
 
   return (
@@ -64,106 +64,113 @@ function MealEntryFormModal(props) {
       aria-labelledby="contained-modal-title-vcenter"
       centered
     >
-      <Modal.Header closeButton>
-        <Modal.Title id="contained-modal-title-vcenter">
-          Enter your Meal details
-        </Modal.Title>
-      </Modal.Header>
-      <Form onSubmit={submitHandler}>
-        <Modal.Body>
-          <h4 className={styles.subheading}>Meal: {menuCtxt.mealName}</h4>
-          <h5 className={styles.subheading}>Timings: {menuCtxt.timings}</h5>
-          <h6 className={styles.subheading}>Menu: {menuCtxt.menu}</h6>
-          <p className={styles.content}>
-            Please mark your availability for this meal here. Food is precious
-            and saving it can feed many people on earth.
-            <strong> Note:</strong> This entry is only for the upcoming meal. To
-            fill further entries, please visit{" "}
-            <Link to="/meal-calendar">Meal Calendar</Link>.
-          </p>
-
-          <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
+      <div className={styles.formContainer}>
+        <Modal.Header closeButton className={styles.header}>
+          <Modal.Title
+            id="contained-modal-title-vcenter"
+            className={styles.heading}
+          >
+            Enter your Meal details
+          </Modal.Title>
+        </Modal.Header>
+        <Form onSubmit={submitHandler}>
+          <Modal.Body>
+            <h4 className={styles.subheading}>Meal: {menuCtxt.mealName}</h4>
+            <h5 className={styles.subheading}>Timings: {menuCtxt.timings}</h5>
+            <h6 className={styles.subheading}>Menu: {menuCtxt.menu}</h6>
+            <p className={styles.content}>
+              Please mark your availability for this meal here. Food is precious
+              and saving it can feed many people on earth.
+              <strong> Note:</strong> This entry is only for the upcoming meal.
+              To fill further entries, please visit{" "}
+              <Link to="/meal-calendar">Meal Calendar</Link>.
+            </p>
+            <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
+              <Row>
+                <Col>
+                  <Form.Label>First Name</Form.Label>
+                  <Form.Control
+                    type="text"
+                    placeholder="First name"
+                    ref={firstNameInputRef}
+                    autoFocus
+                    required
+                  />
+                </Col>
+                <Col>
+                  <Form.Label>Last Name</Form.Label>
+                  <Form.Control
+                    type="text"
+                    placeholder="Last name"
+                    ref={lastNameInputRef}
+                  />
+                </Col>
+              </Row>
+            </Form.Group>
             <Row>
               <Col>
-                <Form.Label>First Name</Form.Label>
-                <Form.Control
-                  type="text"
-                  placeholder="First name"
-                  ref={firstNameInputRef}
-                  autoFocus
-                  required
-                />
+                <Form.Select
+                  defaultValue="Current Year"
+                  aria-label="Default select example"
+                  ref={yearInputRef}
+                >
+                  <option>Current Year</option>
+                  <option value="1">First Year</option>
+                  <option value="2">Second Year</option>
+                  <option value="3">Third Year</option>
+                  <option value="4">Fourth Year</option>
+                </Form.Select>
               </Col>
               <Col>
-                <Form.Label>Last Name</Form.Label>
-                <Form.Control
-                  type="text"
-                  placeholder="Last name"
-                  ref={lastNameInputRef}
-                />
+                <Form.Select
+                  aria-label="Default select example"
+                  ref={hostelNumberInputRef}
+                >
+                  <option>Hostel Number</option>
+                  <option value="1">One</option>
+                  <option value="2">Two</option>
+                  <option value="3">Three</option>
+                  <option value="4">Four</option>
+                </Form.Select>
               </Col>
             </Row>
-          </Form.Group>
-          <Row>
-            <Col>
-              <Form.Select
-                defaultValue="Current Year"
-                aria-label="Default select example"
-                ref={yearInputRef}
-              >
-                <option>Current Year</option>
-                <option value="1">First Year</option>
-                <option value="2">Second Year</option>
-                <option value="3">Third Year</option>
-                <option value="4">Fourth Year</option>
-              </Form.Select>
-            </Col>
-            <Col>
-              <Form.Select
-                aria-label="Default select example"
-                ref={hostelNumberInputRef}
-              >
-                <option>Hostel Number</option>
-                <option value="1">One</option>
-                <option value="2">Two</option>
-                <option value="3">Three</option>
-                <option value="4">Four</option>
-              </Form.Select>
-            </Col>
-          </Row>
-          <div className={styles.toggleStatement}>
-            <Form.Label>Will you eat this meal?</Form.Label>
-            <div className={styles.toggleButton}>
-              <BootstrapSwitchButton
-                checked={true}
-                width="100"
-                onlabel="Yes"
-                offlabel="No"
-                onstyle="success"
-                offstyle="danger"
-                onChange={(checked) => {
-                  setUserResponse(checked);
-                }}
-              />
+            <div className={styles.toggleStatement}>
+              <Form.Label>Will you eat this meal?</Form.Label>
+              <div className={styles.toggleButton}>
+                <BootstrapSwitchButton
+                  checked={true}
+                  width="100"
+                  onlabel="Yes"
+                  offlabel="No"
+                  onstyle="success"
+                  offstyle="danger"
+                  onChange={(checked) => {
+                    setUserResponse(checked);
+                  }}
+                />
+              </div>
             </div>
-          </div>
-          <Form.Select aria-label="Default select example" ref={reasonInputRef}>
-            <option>If NO, specify the reason</option>
-            <option value="1">On Leave</option>
-            <option value="2">Outing</option>
-            <option value="3">Sick</option>
-            <option value="4">I do not want to disclose.</option>
-          </Form.Select>
-        </Modal.Body>
-        <Modal.Footer>
-          <Button className={styles.button} type="reset">
-            Reset
-          </Button>
-          <Button className={styles.button} type="submit">
-            Submit
-          </Button>
-        </Modal.Footer>
-      </Form>
+            <Form.Select
+              aria-label="Default select example"
+              ref={reasonInputRef}
+            >
+              <option>If NO, specify the reason</option>
+              <option value="1">On Leave</option>
+              <option value="2">Outing</option>
+              <option value="3">Sick</option>
+              <option value="4">I do not want to disclose.</option>
+            </Form.Select>
+          </Modal.Body>
+          <Modal.Footer className={styles.formFooter}>
+            <Button className={styles.button} type="reset">
+              Reset
+            </Button>
+            <Button className={styles.button} type="submit">
+              Submit
+            </Button>
+          </Modal.Footer>
+        </Form>
+      </div>
     </Modal>
   );
 }
@@ -174,6 +181,7 @@ function MealEntryForm(props) {
       <MealEntryFormModal
         show={props.isModalOpen}
         onHide={props.closeHandler}
+        showSubmitMessage={props.showSubmitMessage}
       />
     </div>
   );
