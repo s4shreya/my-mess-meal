72628656268ace2372c410beedabe99349463108
unknown <s4shreyadhingra@gmail.com>
2023-03-08 21:35:35 +0530

    added css in upcoming meals card

--- src/components/upcoming-meal/UpcomingMealModal.js
+++ src/components/upcoming-meal/UpcomingMealModal.js
@@ -3,26 +3,42 @@ import { useContext } from "react";
 import Modal from "react-bootstrap/Modal";
 import Button from "react-bootstrap/Button";
 import MenuContext from "../../store/menu-context";
+import styles from "./UpcomingMealModal.module.css";
 
 function UpcomingMealModal(props) {
   const menuCtxt = useContext(MenuContext);
 
   return (
-    <Modal show={props.show} onHide={props.handleClose} centered>
-      <Modal.Header closeButton>
-        <Modal.Title>Upcoming Meal</Modal.Title>
-      </Modal.Header>
-      <Modal.Body>
-        <h1>Upcoming meal: {menuCtxt.mealName}</h1>
-        <h2>Meal timings: {menuCtxt.timings}</h2>
-        <h3>Menu item: {menuCtxt.menu}</h3>
-        <h4>Calories: {menuCtxt.calories} kcal</h4>
-      </Modal.Body>
-      <Modal.Footer>
-        <Button variant="secondary" onClick={props.handleClose}>
-          Close
-        </Button>
-      </Modal.Footer>
+    <Modal
+      show={props.show}
+      onHide={props.handleClose}
+      centered
+      className={styles.modalBody}
+    >
+      <div className={styles.container}>
+        <Modal.Header closeButton className={styles.header}>
+          <Modal.Title className={styles.heading}>
+            Day {menuCtxt.day + 1}
+          </Modal.Title>
+        </Modal.Header>
+        <Modal.Body className={styles.content}>
+          <p>
+            Upcoming meal: {menuCtxt.mealName} <br />
+            Meal timings: {menuCtxt.timings} <br />
+            Menu item: {menuCtxt.menu} <br />
+            Calories: {menuCtxt.calories} kcal
+          </p>
+        </Modal.Body>
+        <Modal.Footer>
+          <Button
+            variant="secondary"
+            onClick={props.handleClose}
+            className={styles.closeButton}
+          >
+            Close
+          </Button>
+        </Modal.Footer>
+      </div>
     </Modal>
   );
 }
